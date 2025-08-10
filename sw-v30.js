0399505478218cfc0a431522379cd2a38db01855
DanielVllmzr <126840617+DanielVllmzr@users.noreply.github.com>
2025-08-10 12:39:28 -0400

    Add files via upload

---
+++ sw-v30.js
@@ -0,0 +1,9 @@
+const CACHE_NAME='gastos-cache-v30-' + Date.now();
+const ASSETS=['./','./index.html','./styles.css','./app.js?v=31','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
