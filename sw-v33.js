c5b9f5987bca9a43596ca0ac54d179378f5abb30
DanielVllmzr <126840617+DanielVllmzr@users.noreply.github.com>
2025-08-10 16:52:38 -0400

    Delete sw-v33.js

--- sw-v33.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='gastos-cache-v33-'+Date.now();
-const ASSETS=['./','./index.html','./styles.css','./app.js?v=35','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
